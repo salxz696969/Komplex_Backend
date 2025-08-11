3e01783334b3361179061faaf0b4417566bd6c1a
visalxz69696969 <saovisal12192005@gmail.com>
2025-08-12 00:33:52 +0700

    forgot cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 console.log(process.env.DATABASE_URL);
@@ -9,3 +11,4 @@ console.log(process.env.DATABASE_URL);
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+
