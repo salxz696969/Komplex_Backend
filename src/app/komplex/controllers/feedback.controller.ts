55a343a4e3efee27515ba752cca06d126cfd66dc
Chanraksa Ory <OCraksa@gmail.com>
2025-08-19 13:48:28 +0700

    removed image upload from feedback

--- src/app/komplex/controllers/feedback.controller.ts
+++ src/app/komplex/controllers/feedback.controller.ts
@@ -5,10 +5,10 @@ import { feedbacks } from "../../../db/models/feedbacks";
 
 export const createFeedback = async (req: Request, res: Response) => {
   try {
-    //! files upload is not implemented yet
+
+    // No files upload for now
     const userId = 1;
     const { content, type } = req.body;
-    // const files = req.files;
     const feedback = await db
       .insert(feedbacks)
       .values({
@@ -20,15 +20,6 @@ export const createFeedback = async (req: Request, res: Response) => {
         updatedAt: new Date(),
       })
       .returning();
-    // if (files) {
-    //   for (const file of Object.values(files)) {
-    //     await db.insert(feedbackMedia).values({
-    //       feedbackId: feedback[0].id,
-    //       publicUrl: file.path,
-    //       mediaType: file.mimetype,
-    //     });
-    //   }
-    // }
     res.json(feedback);
   } catch (error) {
     res.status(500).json({ message: "Failed to create feedback" });
