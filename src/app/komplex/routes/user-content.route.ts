300152f7c30b6ef47cd89805b98c358edde8b723
Chanraksa Ory <OCraksa@gmail.com>
2025-08-24 11:46:53 +0700

    refactored blogs routes

--- src/app/komplex/routes/user-content.route.ts
+++ src/app/komplex/routes/user-content.route.ts
@@ -2,6 +2,11 @@ import { Router } from "express";
 import {
   getAllUserBlogs,
   getBlogById,
+  postBlog,
+  saveBlog,
+  unsaveBlog,
+  updateBlog,
+  deleteBlog,
 } from "../controllers/user-content-blogs.controller";
 import {
   getAllForums,
@@ -19,6 +24,7 @@ import {
   getExerciseById,
 } from "../controllers/user-content-exercises.controller";
 import { getUserContentDashboard } from "../controllers/user-content-dashboard.controller";
+import upload from "../../middleware/upload";
 
 const router = Router();
 
@@ -26,6 +32,18 @@ router.get("/dashboard", getUserContentDashboard);
 
 router.get("/blogs", getAllUserBlogs);
 router.get("/blogs/:id", getBlogById);
+// Create a new blog
+router.post("/blogs", upload.any(), postBlog);
+
+// Save a blog
+router.patch("/blogs/:id/save", saveBlog);
+router.patch("/blogs/:id/unsave", unsaveBlog);
+
+// Update a blog
+router.put("/blogs/:id", upload.any(), updateBlog);
+
+// Delete a blog
+router.delete("/blogs/:id", deleteBlog);
 
 router.get("/forums", getAllForums);
 router.get("/forums/:id", getForumById);
